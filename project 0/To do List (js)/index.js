895ce30f87927752a81e25fb6d43f1e65cb38a03
Anna10PA <puturidzeana0210@gmail.com>
2025-01-20 18:31:33 +0400

    commit

--- project 0/To do List (js)/index.js
+++ project 0/To do List (js)/index.js
@@ -1,14 +1,21 @@
 let task = document.getElementById('text');
 let taskList = document.getElementById('list');
+
 function addTask() {  
-    if (task.value.trim() !== '') {
+    if (task.value !== '') {
+      
       let li = document.createElement('li');
       li.textContent = task.value;
   
       let deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-      deleteButton.onclick = () => li.remove();
-  
+      deleteButton.textContent = 'delete';
+
+      function DeleteTask(){
+        li.remove();
+      } 
+      
+      deleteButton.onclick = DeleteTask
+
       li.appendChild(deleteButton);
       taskList.appendChild(li);
   
